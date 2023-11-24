032cf567dab0f3044281bbb0034a504307058653
Jakub 'lambdaofgod' Bartczuk <bartczukkuba@gmail.com>
2023-11-24 15:56:11 +0100

    missing file

---
+++ tgutil/utils.py
@@ -0,0 +1,14 @@
+from typing import Dict, Any
+import hashlib
+import json
+from fastapi.encoders import jsonable_encoder
+
+
+def dict_hash(dictionary: Dict[str, Any]) -> str:
+    """MD5 hash of a dictionary."""
+    dhash = hashlib.md5()
+    # We need to sort arguments so {'a': 1, 'b': 2} is
+    # the same as {'b': 2, 'a': 1}
+    encoded = json.dumps(jsonable_encoder(dictionary), sort_keys=True).encode()
+    dhash.update(encoded)
+    return dhash.hexdigest()
